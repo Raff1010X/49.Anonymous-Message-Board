4c61f9c33de20981e86434f82bc85359fa9804bd
Rafał Kowalczyk <86784368+Raff1010X@users.noreply.github.com>
2022-12-02 11:49:19 +0100

    Update threadController.js

--- controllers/threadController.js
+++ controllers/threadController.js
@@ -23,10 +23,12 @@ exports.threadGet = async (req, res) => {
     .select('-reported -delete_password -__v')
   
   threads = threads.map(el => {
-    el.replies = el.replies.map(re => {
-      return {_id: re._id, thread_id: re.thread_id, text: re.text,  created_on: re.created_on}
-    })
-    .slice(0, 3)
+    el.replies = el.replies
+      .map(re => {
+        return {_id: re._id, thread_id: re.thread_id, text: re.text,  created_on: re.created_on}})
+      .sort((a, b) => {
+        return new Date(b.created_on) - new Date(a.created_on)})
+      .slice(0, 3)
     return el
   })
   res.json(threads)
